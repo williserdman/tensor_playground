02f460227b90c438add57bf8cacfa1fbaa27dd8a
Willis Erdman <willis.erdman@gmail.com>
2021-05-24 14:34:02 -0700

    put things in folder

--- linear_regression/regression_file.py
+++ linear_regression/regression_file.py
@@ -8,7 +8,7 @@ from sklearn.utils import shuffle
 
 def calculation():
     #Reading this data using pandas, delimiter (the same as sep) is a semicolon for some reason, so it's specified. 
-    data = pd.read_csv("student-mat.csv", sep=';')
+    data = pd.read_csv("~/Documents/coding/tensor_playground/linear_regression/student-mat.csv", sep=';')
     #print(data.head())#The .head() prints only the first 5 values
 
     #Specifying the data that we actually want to process (just these six are going to be taken into account)
@@ -29,29 +29,34 @@ def calculation():
     #This is actually finding the line of best fit (attribute_train=x and label_train=y)
     line_of_best_fit.fit(attribute_train, label_train)
 
-    #Returns accuracy of model
+    #Accuracy of model, varies
     accuracy = line_of_best_fit.score(attribute_test, label_test)
 
-    return accuracy #varies
-
     #With 5 variables it's in 5d space, so there are 5 coefficients
-    print(line_of_best_fit.coef_)
-    print(line_of_best_fit.intercept_)
+    #print(line_of_best_fit.coef_)
+    #print(line_of_best_fit.intercept_)
 
     prediction = line_of_best_fit.predict(attribute_test)
 
     for i, l in enumerate(prediction):
         print(prediction[i], attribute_test[i], label_test[i])
 
-try:
-    times_run = int(input("how many times do you want to run the linear regression model to fin accuracy? (int): "))
-except ValueError:
-    print("thats not an int, silly")
+    return accuracy #varies
 
-avg_accuracy = 0
-for i in range(times_run):
-    avg_accuracy += calculation()
+def accuracy():
+    try:
+        times_run = int(input("how many times do you want to run the linear regression model to fin accuracy? (int): "))
+    except ValueError:
+        print("thats not an int, silly")
 
-avg_accuracy = avg_accuracy/times_run
+    avg_accuracy = 0
+    for i in range(times_run):
+        calc_result = calculation()
+        avg_accuracy += calc_result
 
-print(avg_accuracy*100, '%')#ran 1000 times, 82.5% accuracy on average
+    avg_accuracy = avg_accuracy/times_run
+
+    print("Average Accuracy: ")
+    print(avg_accuracy*100, '%')#ran 1000 times, 82.5% accuracy on average
+    
+calculation()
