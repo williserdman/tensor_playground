6797dbfb13eeb158de1ec8ac3da26a35548eaa7f
Willis Erdman <willis.erdman@gmail.com>
2021-05-24 23:22:36 -0700

    now there is a graph, and one can save good models

--- linear_regression/regression_file.py
+++ linear_regression/regression_file.py
@@ -5,12 +5,15 @@ import numpy as np
 import sklearn
 from sklearn import linear_model
 from sklearn.utils import shuffle
+import matplotlib.pyplot as pyplot
+import pickle
+from matplotlib import style
 
 def calculation():
     #Reading this data using pandas, delimiter (the same as sep) is a semicolon for some reason, so it's specified. 
     data = pd.read_csv("~/Documents/coding/tensor_playground/linear_regression/student-mat.csv", sep=';')
     #print(data.head())#The .head() prints only the first 5 values
-
+    
     #Specifying the data that we actually want to process (just these six are going to be taken into account)
     data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
 
@@ -19,19 +22,35 @@ def calculation():
     attribute_array = np.array(data.drop([to_be_predicted], 1))#why , 1?
     label_array = np.array(data[to_be_predicted])#results, the value of G3
 
-    #Taking attributes and labels (in arrays above) and splitting them up into 4 different arrays. Both attribute_train and label_train will be sections of the respective arrays. The test data will test the accuracy of the algorithm/model. 10% of the data (or 0.1) will be reserved for "test samples"
-    #Where sklearn is taking in two data points, the attribute_array=x and label_array=y it'll be placed on a graph to find the line of best fit
-    #We have more than 2 variables, so have to make a multi-dimensional line of best fit, using multiple variables
     attribute_train, attribute_test, label_train, label_test = sklearn.model_selection.train_test_split(attribute_array, label_array, test_size=0.1)
 
-    line_of_best_fit = linear_model.LinearRegression()
+    '''
+    #The next for loop is creating a model. Once a model is created, doesn't have to be run again, preferable in its own function
+    best_acc = 0 #Getting high accuracy model
+    for i in range(100):
+        #Taking attributes and labels (in arrays above) and splitting them up into 4 different arrays. Both attribute_train and label_train will be sections of the respective arrays. The test data will test the accuracy of the algorithm/model. 10% of the data (or 0.1) will be reserved for "test samples"
+        #Where sklearn is taking in two data points, the attribute_array=x and label_array=y it'll be placed on a graph to find the line of best fit
+        #We have more than 2 variables, so have to make a multi-dimensional line of best fit, using multiple variables
+        attribute_train, attribute_test, label_train, label_test = sklearn.model_selection.train_test_split(attribute_array, label_array, test_size=0.1)
 
-    #This is actually finding the line of best fit (attribute_train=x and label_train=y)
-    line_of_best_fit.fit(attribute_train, label_train)
+        line_of_best_fit = linear_model.LinearRegression()
 
-    #Accuracy of model, varies
-    accuracy = line_of_best_fit.score(attribute_test, label_test)
+        #This is actually finding the line of best fit (attribute_train=x and label_train=y)
+        line_of_best_fit.fit(attribute_train, label_train)
 
+        #Accuracy of model, varies
+        accuracy = line_of_best_fit.score(attribute_test, label_test)
+        print(accuracy)
+
+        if accuracy > best_acc:
+            best_acc = accuracy
+            #Saving our model, if there's one with a really high accuracy for example, as they all train slightly differently. Or for larger data sets you don't want to have to re-train your model.
+            with open("studentmodel.pickle", 'wb') as pickle_file:
+                pickle.dump(line_of_best_fit, pickle_file)'''
+    #print(best_acc)
+    with open("studentmodel.pickle", 'rb') as pickle_input:
+        line_of_best_fit = pickle.load(pickle_input)
+    
     #With 5 variables it's in 5d space, so there are 5 coefficients
     #print(line_of_best_fit.coef_)
     #print(line_of_best_fit.intercept_)
@@ -41,22 +60,12 @@ def calculation():
     for i, l in enumerate(prediction):
         print(prediction[i], attribute_test[i], label_test[i])
 
-    return accuracy #varies
+    one_attribute = "G1"
+    style.use("ggplot")
+    #Creates a scatterplot, where one attribute is the x val. The y val is the label/result/G3
+    pyplot.scatter(data[one_attribute], data["G3"])
+    pyplot.xlabel(one_attribute)
+    pyplot.ylabel("Final Grade")
+    pyplot.show()
 
-def accuracy():
-    try:
-        times_run = int(input("how many times do you want to run the linear regression model to fin accuracy? (int): "))
-    except ValueError:
-        print("thats not an int, silly")
-
-    avg_accuracy = 0
-    for i in range(times_run):
-        calc_result = calculation()
-        avg_accuracy += calc_result
-
-    avg_accuracy = avg_accuracy/times_run
-
-    print("Average Accuracy: ")
-    print(avg_accuracy*100, '%')#ran 1000 times, 82.5% accuracy on average
-    
 calculation()
