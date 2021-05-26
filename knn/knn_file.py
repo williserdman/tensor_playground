f183a818047133be8316a4f325d1970c0b58ea5d
Willis Erdman <willis.erdman@gmail.com>
2021-05-25 23:12:37 -0700

    done with knn, learned a lot

--- knn/knn_file.py
+++ knn/knn_file.py
@@ -1,4 +1,5 @@
 
+#k nearest neighbors, k is a variable, the number of neighbors to look for. this is very computationally heavy, and the time it takes to compute will linearly increase
 import pandas as pd
 import sklearn
 from sklearn.utils import shuffle
@@ -17,18 +18,30 @@ door = le.fit_transform(list(data["door"]))
 persons = le.fit_transform(list(data["persons"]))
 lug_boot = le.fit_transform(list(data["lug_boot"]))
 safety = le.fit_transform(list(data["safety"]))
-clss = le.fit_transform(list(data["class"]))
+stlye = le.fit_transform(list(data["style"]))
 
-predict = "class"
+predict = "stlye"
 
 #Taking these arrays and converting them into lists
 x = list(zip(buying, maint, door, persons, lug_boot, safety))
-y = list(clss)#y is result
+y = list(stlye)#y is result
 
+#Anything with train in it, are used for training. x_train would be the input for training, and to check its work y_train would be used. That means that the model already knows those values, so should have 100% accuracy. x_test and y_test follow the same idea, but the model has never seen them before. 
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
 
-print(x_train)
-print(x_test)
-print(y_train)
-print(y_test)
+model = KNeighborsClassifier(n_neighbors = 9)
+
+model.fit(x_train, y_train)
+acc = model.score(x_test, y_test)
+print(acc)
+
+prediction = model.predict(x_test)
+names = ["unacc", "acc", "good", "vgood"]
+
+for i, l in enumerate(x_test):
+    print(names[prediction[i]], end = ' | ')
+    print(l, end = ' | ')
+    print(names[y_test[i]], end = ' | ')
+    model.kneighbors([x_test[i]], 9)#kneighbors takes a 2 dimensional array, so we have to turn our input into one
+
 
